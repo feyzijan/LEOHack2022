f1bca948654cf6f49bb75817cdc8785e813baf99
feyzijan <feyzican4@gmail.com>
2022-02-13 18:18:39 +0000

    Code clean-up, comments, and organisation- functionality unchanged
    Performance of model unchanced
    Made more easy to configure for diferent challenges by just changing one parameter

--- team/team_controller.py
+++ team/team_controller.py
@@ -1,49 +1,80 @@
 from sat_controller import SatControllerInterface, sat_msgs
-
 import math
 
-# Team code is written as an implementation of various methods
-# within the the generic SatControllerInterface class.
-# If you would like to see how this class works, look in sat_control/sat_controller
-
-# Specifically, init, run, and reset
 
 class TeamController(SatControllerInterface):
-    """ Team control code """
 
     def team_init(self):
-        """ Runs any team based initialization """
         self.logger.info("Initialisation started")
-        # Run any initialization you need
 
+        self.challenge_no = 4 # Used to toggle manually resetting theta for challenge 2 after it exceeds n*2*pi
+
+        # Integral Errors initialisation
+        # The integral error is approximated by summing up all previous positional errors 
         self.e_x_integ = 0.0
         self.e_y_integ = 0.0
         self.e_o_integ = 0.0
 
+        # Variables used to store the previous posititonal error for error derivative calculation
         self.e_x_prev = 0.0
         self.e_y_prev = 0.0
         self.e_o_prev = 0.0
 
+        # Derivative Errors initialisation
+        # Derivative Error is approximated by subtracting the previous error from the current
         self.e_x_dt = 0.0
         self.e_y_dt = 0.0
         self.e_o_dt = 0.0
 
-        self.v_x_max_log = 0
-        self.v_y_max_log = 0
-        self.v_z_max_log = 0
+        # Note: Derivative and Integral errors are approximates, and due to the small timesteps you may need lower Gain 
+        # values for them not to be ridiculously high
 
-        # Example of persistant data
+        # Maximum velocities achieved in each direction - to be logged
+        self.v_x_max_log = 0.0
+        self.v_y_max_log = 0.0
+        self.v_z_max_log = 0.0
+        self.v_o_max_log = 0.0
+
+        # Gain parameters for different challenges
+        # These are tuned so that different types of approaches can be attempted: 
+        # eg. overshoot in y direction then approach while in a orbital like motion
+
+        # Positional Error Proportional Gain
+        self.Kp = [[8.0,  6.0,  8.0 ], 
+              [6.0,  10.5,  8.0],
+              [0.8,  0.5,  5.0 ],
+              [8.0,  6.0,  8.0 ] ]
+
+        # Positional Error Derivative (approximation) Gain
+        self.Kd = [[0.0, 0.0, 0.0 ],
+              [0.0, 0.0, 0.0 ],
+              [0.5, 0.0, 0.0 ],
+              [0.0, 0.0, 0.0 ],]
+
+        # Positional Error Integral (approximation) Gain
+        self.Ki = [[0.01, 0.01,  0.01 ],
+              [0.01, 0.03,  0.01 ],
+              [0.003, 0.003, 0.2 ],
+              [0.01, 0.01,  0.01 ] ]
+
+        # Moving Satellite Velocity Gain - for damping
+        self.Kdy = [[20.0,  20.0,  20.0],
+              [20.0,  20.0,  20.0 ],
+              [20.0,  20.0,  20.0 ],
+              [20.0,  20.0,  20.0 ] ]
+        
+        # Overall gain 
+        self.K = [[1.0,  1.0,  1.0],
+             [1.0,  1.0,  1.0],
+             [1.0,  1.0,  1.0],
+             [1.0,  1.0,  1.0] ]
+
         self.counter = 0
-
-        # Example of logging
-        self.logger.info("Initialized :)")
-        self.logger.warning("Warning...")
-        self.logger.error("Error!")
 
         # Update team info
         team_info = sat_msgs.TeamInfo()
         team_info.teamName = "Team7"
-        team_info.teamID = 1111
+        team_info.teamID = 7777
 
         # Return team info
         return team_info
@@ -51,42 +82,59 @@ class TeamController(SatControllerInterface):
     def team_run(self, system_state: sat_msgs.SystemState, satellite_state: sat_msgs.SatelliteState, dead_sat_state: sat_msgs.SatelliteState) -> sat_msgs.ControlMessage:
         """ Takes in a system state, satellite state """
 
-        print(dead_sat_state)
+        n = self.challenge_no - 1 
 
-        # Get timedelta from elapsed time
+        # Log time and counter
         elapsed_time = system_state.elapsedTime.ToTimedelta()
         self.logger.info(f'Elapsed time: {elapsed_time}')
-
-        # Example of persistant data
         self.counter += 1
-
-        # Example of logging
         self.logger.info(f'Counter value: {self.counter}')
         
 
         # Create a thrust command message
         control_message = sat_msgs.ControlMessage()
 
-        ##### Get errors
-
-        # Positional Error !!! Paste in relevant challenge's code
-        x_offset = -0.25 * math.sin(dead_sat_state.pose.theta) - 0.03
-        y_offset = 0.25 * math.cos(dead_sat_state.pose.theta) + 0.05
-        o_offset = math.pi
-        e_x = dead_sat_state.pose.x - satellite_state.pose.x - x_offset
-        e_y = dead_sat_state.pose.y - satellite_state.pose.y - y_offset # 0.25
-        e_o = dead_sat_state.pose.theta - satellite_state.pose.theta -o_offset
-        e_z = math.sqrt(e_x * e_x + (e_y + y_offset) * (e_y + y_offset))
-        #################################################
+        # Positional Errors and necessary offsets for docking  
+        # Note: In the below lines the required x and y offsets are slightly modified based on the control parameters 
+        # An ideal selection of control parameters should make the small adjustments below unnecessary, but life only long enough
+        # to try so many control parameters
+        # So x and y positional errors are slightly modifeid to compensate for non-optimal parameter choices
+         
+         
+        # X offset to subtract from x error
+        x_offset = -0.25 * math.sin(dead_sat_state.pose.theta) 
         
-        if self.challenge_no == 2: # To make up for inverse angles
+        # Challenge specific modifications
+        if n == 1: # To make up for theta being weird in challenge 2
+             x_offset *= -1
+        elif n == 3:
+            x_offset -= 0.03
+        
+        # Y offset to subtract from y error
+        y_offset = 0.25 * math.cos(dead_sat_state.pose.theta) 
+
+        # Challenge specific modifications
+        if n == 1:
+            y_offset += 0.03
+        elif n == 2 or n == 3:
+            y_offset += 0.05
+
+
+        # Omega offset (so satellites face together)
+        o_offset = math.pi
+
+        # Below code is used to make up for theta not resetting to zero after teaching n*2*pi in Challenge 3
+        if self.challenge_no == 2: 
             if dead_sat_state.pose.theta < -2*math.pi:
                 dead_sat_state.pose.theta += 2* math.pi
             elif dead_sat_state.pose.theta > 2*math.pi:
                 dead_sat_state.pose.theta -= 2* math.pi
 
+        # Calculate positional errors
+        e_x = dead_sat_state.pose.x - satellite_state.pose.x - x_offset
+        e_y = dead_sat_state.pose.y - satellite_state.pose.y - y_offset 
+        e_z = math.sqrt(e_x * e_x + (e_y + y_offset) * (e_y + y_offset)) 
         e_o = dead_sat_state.pose.theta - satellite_state.pose.theta -o_offset
-        e_z = math.sqrt(e_x * e_x + (e_y + y_offset) * (e_y + y_offset))
 
         # Velocities
         v_x = satellite_state.twist.v_x 
@@ -94,57 +142,45 @@ class TeamController(SatControllerInterface):
         v_o = satellite_state.twist.omega 
         v_z = math.sqrt(v_x * v_x + v_y * v_y)
 
-
-
-        ####### PD controller ##########
-        Kp = [8.0,  6.0,  8.0]
-        Kd = [0.1,0.1,0.1]
-        Ki = [0.01, 0.01,  0.01]
-        Kd = [0.0,  0.0,  0.0]
-        Kdy = [20.0,  20.0,  20.0]
-
-        K = [1.0,  1.0,  1.0]
-
-
+        # Approximate Integral Errors
         self.e_x_integ += e_x 
         self.e_y_integ += e_y 
         self.e_o_integ += e_o 
 
+        # Approximate Derivative Errors
         self.e_x_dt = e_x - self.e_x_prev 
         self.e_y_dt = e_y - self.e_y_prev 
         self.e_o_dt = e_o - self.e_o_prev 
 
-        # Update previous error values
+        # Update variables that store previous positional errors
         self.e_x_prev = e_x
         self.e_y_prev = e_y
         self.e_o_prev = e_o
-
-
-        control_message.thrust.f_x = K[0] * (Kp[0] * (e_x)  + Ki[0] * self.e_x_integ + Kd[0] * self.e_x_dt - Kdy[0] * v_x )
-        control_message.thrust.f_y = K[1] * (Kp[1] * (e_y)  + Ki[1] * self.e_y_integ + Kd[1] * self.e_y_dt - Kdy[1] * v_y )
-        control_message.thrust.tau = K[2] * (Kp[2] * (e_o)  + Ki[2] * self.e_o_integ + Kd[2] * self.e_o_dt - Kdy[2] * v_o )
         
-        #################################
-
+        ### PID Control Code
+        control_message.thrust.f_x = self.K[n][0] * (self.Kp[n][0] * (e_x)  + self.Ki[n][0] * self.e_x_integ + self.Kd[n][0] * self.e_x_dt - self.Kdy[n][0] * v_x )
+        control_message.thrust.f_y = self.K[n][1] * (self.Kp[n][1] * (e_y)  + self.Ki[n][1] * self.e_y_integ + self.Kd[n][1] * self.e_y_dt - self.Kdy[n][1] * v_y )
+        control_message.thrust.tau = self.K[n][2] * (self.Kp[n][2] * (e_o)  + self.Ki[n][2] * self.e_o_integ + self.Kd[n][2] * self.e_o_dt - self.Kdy[n][2] * v_o )
+        
 
         ########## Enforcing  velocity constraints ################
 
-        # Initial high values for thrust to be updated
+        # Set initial threshold values for thrust - to be updated
         thrust_x_max, thrust_y_max, thrust_tau_max = 10,10,10
         
         # Decide on state : 1- Far away, 2- Within 0.5m radius, 3- Docking
         if e_z > 0.5: 
             v_max_rel = 1
-        elif e_z > 0.075: # Close
+        elif e_z > 0.075:
             self.logger.info("--- Within sphere---")
             v_max_rel = 0.2
-        else: # Real close
+        else: 
             self.logger.info("---Just about to dock---")
             v_max_rel = 0.05
             #w_max_rel = 0.1 
         ##################
         
-        # Get absolute velocities in x, y, z
+        # Get absolute velocities in x, y, z, o for both satellites
         v_x_sat, v_y_sat, v_o_sat = satellite_state.twist.v_x, satellite_state.twist.v_y, satellite_state.twist.omega
         v_x_dsat, v_y_dsat, v_o_sat = dead_sat_state.twist.v_x, dead_sat_state.twist.v_y, dead_sat_state.twist.omega
         v_z_sat_abs = math.sqrt(v_x_sat * v_x_sat + (v_y_sat) * (v_y_sat))
@@ -153,15 +189,17 @@ class TeamController(SatControllerInterface):
         # Get relative velocity in z
         v_z_rel = abs(v_z_sat_abs - v_z_dsat_abs)
 
-        if v_z_rel < v_max_rel:
+        if v_z_rel < v_max_rel: # Velocity constraint not met
+            # Basically limit x and y velocities so their sum of squares cant go over the limit
+            # Then apply the approximate maximum thrusts to limit the velocity 
             v_x_allowed = v_x_dsat + v_max_rel / 2
             v_y_allowed = v_y_dsat + v_max_rel / 2
     
             thrust_x_max = ( v_x_allowed - v_x_sat) * 20
-            thrust_y_max = (v_y_allowed - v_y_sat) * 20
+            thrust_y_max = ( v_y_allowed - v_y_sat) * 20
 
 
-        # Wait up for simulation to load after starting
+        # Wait up a bit for simulation to load after starting
         if self.counter <15:
             control_message.thrust.f_x, control_message.thrust.f_y, control_message.thrust.tau = 0, 0, 0
         
@@ -188,8 +226,12 @@ class TeamController(SatControllerInterface):
         # Return control message
         return control_message
 
+    def team_reset(self) -> None:
+        # Run any reset code
+        pass
 
-    # In function form - not used
+
+        # Max Velocity Enforcement in Function Form - Not used
     def check_max_vel(self,satellite_state: sat_msgs.SatelliteState, dead_sat_state: sat_msgs.SatelliteState):
         state = 0
         """ Decide where the satelite is
@@ -244,8 +286,3 @@ class TeamController(SatControllerInterface):
             thrust_y_allowed = (v_y_sat- v_y_allowed ) * 20
 
         return thrust_x_allowed, thrust_y_allowed
-
-
-    def team_reset(self) -> None:
-        # Run any reset code
-        pass
